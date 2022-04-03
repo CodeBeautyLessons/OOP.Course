fdaf576bba855b506cef271eabe9bdcd6ea3a903
Vladimir Novikov <novikovvlal@gmail.com>
2022-04-03 09:27:53 +0300

    C++ interfaces

---
+++ 3. AbstractionsExplained/3. AbstractionsExplained.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Smartphone {
+public:
+    virtual void TakeASelfie() = 0;
+    virtual void MakeACall() = 0;
+};
+
+class Android : public Smartphone {
+public:
+    void TakeASelfie() {
+        cout << "Android selfie\n";
+    }
+
+    void MakeACall() {
+        cout << "Android calling\n";
+    }
+};
+
+class IPhone : public Smartphone {
+public:
+    void TakeASelfie() {
+        cout << "IPhone selfie\n";
+    }
+
+    void MakeACall() {
+        cout << "IPhone calling\n";
+    }
+};
+
+int main()
+{
+    Smartphone* s1 = new Android();
+    s1->TakeASelfie();
+    s1->MakeACall();
+
+    return 0;
+}
