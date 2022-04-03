5453bac8d3c5dcc75583bb307ae88e09acb1d9ba
Vladimir Novikov <novikovvlal@gmail.com>
2022-04-03 10:57:51 +0300

    Friend classes and functions

---
+++ 5. FriendFunctions/5. FriendFunctions.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class EquilateralTriangle {
+private:
+    float a;
+    float circumference;
+    float area;
+public:
+    void SetA(float length) {
+        a = length;
+
+        circumference = a * 3;
+        area = (sqrt(3) * a * a) / 4;
+    }
+
+    friend void PrintResults(const EquilateralTriangle&);
+    friend class Homework;
+};
+
+void PrintResults(const EquilateralTriangle& et) {
+    cout << "circumference = " << et.circumference << endl;
+    cout << "area = " << et.area << endl;
+}
+
+class Homework {
+public:
+    static void PrintResults(const EquilateralTriangle& et) {
+        cout << "circumference = " << et.circumference << endl;
+        cout << "area = " << et.area << endl;
+    }
+};
+
+int main()
+{
+    EquilateralTriangle et;
+    et.SetA(3);
+
+    PrintResults(et);
+    Homework::PrintResults(et);
+
+    system("pause>null");
+}
---
+++ 5. FriendFunctionsHomework/5. FriendFunctionsHomework.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <string>
+#include <ostream>
+#include <list>
+
+using namespace std;
+
+class YouTubeChannel {
+private:
+    string Name;
+    int SubscribersCount;
+
+public:
+    YouTubeChannel(string name, int subscribersCount) {
+        Name = name;
+        SubscribersCount = subscribersCount;
+    }
+
+    bool operator==(const YouTubeChannel& youTubeChannel) const {
+        return this->Name == youTubeChannel.Name;
+    }
+
+    friend ostream& operator << (ostream& stream, YouTubeChannel& youTubeChannel);
+};
+
+ostream& operator << (ostream& stream, YouTubeChannel& youTubeChannel) {
+    stream << "Name: " << youTubeChannel.Name << endl
+        << "Subscribers count: " << youTubeChannel.SubscribersCount << endl;
+
+    return stream;
+}
+
+class MyCollection {
+private:
+    list<YouTubeChannel> MyChannels;
+
+public:
+    void operator+=(YouTubeChannel& youTubeChannel) {
+        this->MyChannels.push_back(youTubeChannel);
+    }
+
+    void operator-=(YouTubeChannel& youTubeChannel) {
+        this->MyChannels.remove(youTubeChannel);
+    }
+
+    friend ostream& operator << (ostream& stream, MyCollection& myCollection);
+};
+
+ostream& operator << (ostream& stream, MyCollection& myCollection) {
+    for (auto ytChannel : myCollection.MyChannels) {
+        stream << ytChannel << endl;
+    }
+
+    return stream;
+}
+
+int main()
+{
+    YouTubeChannel yt1 = YouTubeChannel("CodeBeauty", 75000);
+    YouTubeChannel yt2 = YouTubeChannel("MySecondChannel", 80000);
+    cout << yt1 << yt2;
+
+    //also works fine
+    operator<<(cout, yt2);
+
+    MyCollection myCollection;
+
+    myCollection += yt1;
+    myCollection += yt2;
+    myCollection -= yt2;
+
+    cout << myCollection;
+
+    cin.get();
+}
